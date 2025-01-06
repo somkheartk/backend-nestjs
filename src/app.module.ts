b3ab9f51f4e622429188a38b3a304f7e5df01f0e
somkheartk <43743072+somkheartk@users.noreply.github.com>
2025-01-07 00:51:16 +0700

    first deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
@@ -9,7 +10,10 @@ import { UserController } from './user/user.controller';
 import { UserModule as UserModuleTsModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModuleTsModule, UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+     UserModuleTsModule, UserModule],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
